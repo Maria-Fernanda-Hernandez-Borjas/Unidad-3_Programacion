5fe569672660919aa89cc68e030ecd88d79627c8
Maria-Fernanda-Hernandez-Borjas <maryfherherbo@gmail.com>
2025-04-04 11:24:51 -0500

    Add files via upload

---
+++ Práctica 1/Form1.cs
@@ -0,0 +1,25 @@
+namespace _1
+{
+    public partial class GIF_animado : Form
+    {
+        public GIF_animado()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
